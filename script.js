ab0530120f8f99d029018bc7ef12d74d19385ab1
Netra21904 <128806086+Netra21904@users.noreply.github.com>
2025-03-06 12:27:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+document.getElementById('btn').addEventlistener('click', () => {
+	const inputElement = document.getElementById('ip');
+	const outputEement = document.getElementById('output');
+
+delay(2000, inputValue)
+	.then(value => {
+		outputElement.textContent = Result: <number>;
+		return delay(2000, value * 2);
+	})
+	.then(value => {
+		outputElement.textContent = Result: <number>;
+		return delay(1000, value - 3);
+	})
+	.then(value => {
+		outputElement.textContent = Result: <number>;
+		return delay(1000, value / 2);
+	})
+	.then(value => {
+		outElement.textContent = Final Result: <number>;
+	})
+	.catch(error => {
+		console.error('An error occured:', error);
+		outputElement.textContent = 'An error occurred.';
+	});
+});
